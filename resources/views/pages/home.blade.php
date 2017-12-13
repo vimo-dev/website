95ca41479b5698b889a13fa0ed221a4db8b24d3f
abi-dev <benjamin.ableidinger@gmail.com>
2017-12-13 22:36:56 +0100

    testing dev workflow

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,14 +4,6 @@
 
 @section('content')
 
-    <div class="flex-center position-ref full-height">
 
-        <div class="content">
-            <div class="title m-b-md">
-                Hallo
-            </div>
-        </div>
-        
-    </div>
-
+    
 @endsection
